6400c62229a0ac2412ddceb08fbe6aac0222de1e
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-04-28 11:09:56 +0700

    remove unnecesary comment

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -22,22 +22,6 @@ local config = function ()
       dashboard.button( "s", "  > Settings" , ":e $MYVIMRC<CR>"),
   }
 
-  -- Set footer
-  --   NOTE: This is currently a feature in my fork of alpha-nvim (opened PR #21, will update snippet if added to main)
-  --   To see test this yourself, add the function as a dependecy in packer and uncomment the footer lines
-  --   ```init.lua
-  --   return require('packer').startup(function()
-  --       use 'wbthomason/packer.nvim'
-  --       use {
-  --           'goolord/alpha-nvim', branch = 'feature/startify-fortune',
-  --           requires = {'BlakeJC94/alpha-nvim-fortune'},
-  --           config = function() require("config.alpha") end
-  --       }
-  --   end)
-  --   ```
-  -- local fortune = require("alpha.fortune") 
-  -- dashboard.section.footer.val = fortune()
-
   -- Send config to alpha
   alpha.setup(dashboard.opts)
 
