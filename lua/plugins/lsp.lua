39f5a6a18a934f66f6567e8116eaa080c7e5c912
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-05-16 20:55:15 +0700

    add border on hover lsp diagnostic

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -55,12 +55,17 @@ local config = function ()
 
     local handlers = {
       ["textDocument/publishDiagnostics"] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics, {
-        underline = true,
-        virtual_text = false,
-        signs = true,
-        update_in_insert = false,
-      }
+        vim.lsp.diagnostic.on_publish_diagnostics, {
+          underline = true,
+          virtual_text = false,
+          signs = true,
+          update_in_insert = false,
+        }
+      ),
+      ["textDocument/hover"] = vim.lsp.with(
+        vim.lsp.handlers.hover, {
+          border = "rounded",
+        }
       ),
     }
 
