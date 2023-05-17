912d909a37bd1a0747ba1424309ef500e0ecd466
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-05-17 14:54:10 +0700

    change visual mode and current word hl color

--- lua/brew/colors.lua
+++ lua/brew/colors.lua
@@ -2,6 +2,6 @@ vim.cmd.colorscheme('sonokai')
 
 local set_hl = vim.api.nvim_set_hl
 -- overide hl colors from default theme below
-set_hl(0, "Visual", { link = "DiffAdd" })
+set_hl(0, "Visual", { bg = "#346273" })
 set_hl(0, 'LocalMatchHighlight', { italic = true, bg='#2b2f3c' })
-set_hl(0, 'LocalCurrentWord', { bold = true, bg='#5d616c' })
+set_hl(0, 'LocalCurrentWord', { bold = true, bg='#3c404e' })
