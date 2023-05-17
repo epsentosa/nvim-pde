8ebe944a970c9cfa30a59eba602b7a5d89085e03
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-05-17 09:55:36 +0700

    reindent lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,55 +19,55 @@ local config = function ()
           },
         },
       },
+    gopls = {
       gopls = {
-        gopls = {
-          analyses = { unusedparams = true },
-          staticcheck = true,
-        },
+        analyses = { unusedparams = true },
+        staticcheck = true,
       },
-      lua_ls = {
-        Lua = {
-          workspace = { checkThirdParty = false },
-          telemetry = { enable = false },
-        },
+    },
+    lua_ls = {
+      Lua = {
+        workspace = { checkThirdParty = false },
+        telemetry = { enable = false },
       },
-      jdtls = {},
-    }
+    },
+    jdtls = {},
+  }
 
-    -- Setup neovim lua configuration
-    require('neodev').setup()
-    --
-    -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+  -- Setup neovim lua configuration
+  require('neodev').setup()
+  --
+  -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-    -- Setup mason so it can manage external tooling
-    require('mason').setup({
-      ui = { border = "rounded" }
-    })
+  -- Setup mason so it can manage external tooling
+  require('mason').setup({
+    ui = { border = "rounded" }
+  })
 
-    -- Ensure the servers above are installed
-    local mason_lspconfig = require 'mason-lspconfig'
+  -- Ensure the servers above are installed
+  local mason_lspconfig = require 'mason-lspconfig'
 
-    mason_lspconfig.setup {
-      ensure_installed = vim.tbl_keys(servers),
-    }
+  mason_lspconfig.setup {
+    ensure_installed = vim.tbl_keys(servers),
+  }
 
-    local handlers = {
-      ["textDocument/publishDiagnostics"] = vim.lsp.with(
-        vim.lsp.diagnostic.on_publish_diagnostics, {
-          underline = true,
-          virtual_text = false,
-          signs = true,
-          update_in_insert = false,
-        }
-      ),
-      ["textDocument/hover"] = vim.lsp.with(
-        vim.lsp.handlers.hover, {
-          border = "rounded",
-        }
-      ),
-    }
+  local handlers = {
+    ["textDocument/publishDiagnostics"] = vim.lsp.with(
+      vim.lsp.diagnostic.on_publish_diagnostics, {
+        underline = true,
+        virtual_text = false,
+        signs = true,
+        update_in_insert = false,
+      }
+    ),
+    ["textDocument/hover"] = vim.lsp.with(
+      vim.lsp.handlers.hover, {
+        border = "rounded",
+      }
+    ),
+  }
 
   local navic = require("nvim-navic")
 
@@ -92,40 +92,43 @@ local config = function ()
     end, { desc = 'Format current buffer with LSP' })
   end
 
-    mason_lspconfig.setup_handlers {
-      function(server_name)
-        require('lspconfig')[server_name].setup {
-          capabilities = capabilities,
-          on_attach = on_attach,
-          settings = servers[server_name],
-          handlers = handlers,
-        }
-      end,
-    }
+  mason_lspconfig.setup_handlers {
+    function(server_name)
+      require('lspconfig')[server_name].setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = servers[server_name],
+        handlers = handlers,
+      }
+    end,
+  }
 
-    -- Handling clangd warning: multiple different client offset_encodings detected for buffer, this is not supported yet
-    capabilities.offsetEncoding = { "utf-16" }
-    require('lspconfig').clangd.setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      handlers = handlers,
-    })
+  -- Handling clangd warning: multiple different client offset_encodings detected for buffer, this is not supported yet
+  capabilities.offsetEncoding = { "utf-16" }
+  require('lspconfig').clangd.setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+    handlers = handlers,
+  })
 
-    -- Change Sign Symbol
-    local function lspSymbol(name, icon)
-      vim.fn.sign_define(
-      'DiagnosticSign' .. name,{
-        text = icon,
-        numhl = 'DiagnosticDefault' .. name,
-        texthl = 'DiagnosticSign' .. name
-        }
-      )
-    end
-    lspSymbol('Error', '')
-    lspSymbol('Info', '')
-    lspSymbol('Hint', '')
-    lspSymbol('Info', '')
-    lspSymbol('Warn', '')
+  -- Add border on LspInfo command
+  require('lspconfig.ui.windows').default_options.border = 'single'
+
+  -- Change Sign Symbol
+  local function lspSymbol(name, icon)
+    vim.fn.sign_define(
+    'DiagnosticSign' .. name,{
+      text = icon,
+      numhl = 'DiagnosticDefault' .. name,
+      texthl = 'DiagnosticSign' .. name
+      }
+    )
+  end
+  lspSymbol('Error', '')
+  lspSymbol('Info', '')
+  lspSymbol('Hint', '')
+  lspSymbol('Info', '')
+  lspSymbol('Warn', '')
 end
 
 return {
