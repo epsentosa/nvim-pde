558423977609af8292cc46afc1d7255eeff63da8
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-05-18 22:05:59 +0700

    set keymap on codeium

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,4 +1,11 @@
 return {
   'Exafunction/codeium.vim',
   cmd = 'CodeiumEnable',
+  config = function ()
+    -- Change '<C-g>' here to any keycode you like.
+    vim.keymap.set('i', '<Tab>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+    vim.keymap.set('i', '<M-[>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+    vim.keymap.set('i', '<M-]>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+    vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+  end
 }
