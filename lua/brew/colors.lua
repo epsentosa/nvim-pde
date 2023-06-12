e8d9433ff97b1361e5db4f8dc6f276049afedaa4
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-06-12 23:57:24 +0700

    set custom color line number

--- lua/brew/colors.lua
+++ lua/brew/colors.lua
@@ -5,3 +5,6 @@ local set_hl = vim.api.nvim_set_hl
 set_hl(0, "Visual", { bg = "#346273" })
 set_hl(0, 'LocalMatchHighlight', { italic = true, bg='#2b2f3c' })
 set_hl(0, 'LocalCurrentWord', { bold = true, bg='#3c404e' })
+set_hl(0, 'LineNrAbove', { fg='#5a6477' })
+set_hl(0, 'LineNr', { fg='yellow' })
+set_hl(0, 'LineNrBelow', { fg='#5a6477' })
