3b774a289263854a72e51d8ccdab7ff1cb3a0cc5
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-07-05 13:21:21 +0700

    change keymap hop to s

--- lua/brew/keymaps.lua
+++ lua/brew/keymaps.lua
@@ -78,7 +78,7 @@ hop_keymap = function ()
   keymap('', 'T', function()
     hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
   end, {remap=true})
-  keymap('n', '<C-h>', vim.cmd.HopWord )
+  keymap('n', 's', vim.cmd.HopWord )
 end
 
 -- Telescope
