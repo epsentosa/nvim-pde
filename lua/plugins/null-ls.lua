3051c9f07388a81497884b2d4f3ae0e13339484a
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-07-17 20:44:20 +0700

    add diagnostic golangci_lint in null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -2,6 +2,7 @@ local config = function ()
   local null_ls = require("null-ls")
 
   local formatting = null_ls.builtins.formatting
+  local diagnostics = null_ls.builtins.diagnostics
 
   local sources = {
     formatting.lua_format.with({
@@ -21,6 +22,9 @@ local config = function ()
         "-style={IndentWidth: 4}"
       }
     }),
+    diagnostics.golangci_lint,
+    -- diagnostics.flake8,
+    -- diagnostics.pycodestyle,
   }
 
   null_ls.setup({
