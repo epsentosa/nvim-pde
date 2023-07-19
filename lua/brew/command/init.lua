38b934c9d34c5945c972d9297fbbec26d6574ec7
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-07-19 21:16:19 +0700

    add lua function for neovim plugin development

--- lua/brew/command/init.lua
+++ lua/brew/command/init.lua
@@ -2,5 +2,6 @@ require('brew.command.goplayground')
 require('brew.command.luaplayground')
 require('brew.command.pythonplayground')
 require('brew.command.formatting')
+require('brew.command.dev')
 -- TODO
 -- create command that close goplayground directry
