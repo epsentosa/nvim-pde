b0574e8defe75dc1bff52b2e5b930cf376991c5d
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-07-20 21:27:28 +0700

    make nvim cmp config separated from lazy plugin

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,64 +1,3 @@
-local config = function ()
-  -- nvim-cmp setup
-  local cmp = require 'cmp'
-  local luasnip = require 'luasnip'
-  local lspkind = require 'lspkind'
-
-  -- load vs-code like snippets from plugins (e.g. friendly-snippets)
-  require("luasnip/loaders/from_vscode").lazy_load()
-
-  cmp.setup {
-    window = {
-        documentation = {
-            border = 'rounded'
-        },
-        completion = {
-            border = 'rounded'
-        },
-    },
-    snippet = {
-      expand = function(args)
-        luasnip.lsp_expand(args.body)
-      end,
-    },
-    mapping = cmp.mapping.preset.insert {
-      ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-      ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete {}, -- on mac, keyboard wont work on ABC
-      ['<CR>'] = cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      },
-    },
-    sources = cmp.config.sources({
-      { name = 'nvim_lua' },
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' },
-      { name = 'path' },
-      { name = 'buffer',  keyword_length = 3 },
-    }),
-    formatting = {
-      format = lspkind.cmp_format({
-        with_text = true,
-        menu = {
-          buffer = "[buf]",
-          nvim_lsp = "[LSP]",
-          nvim_lua = "[api]",
-          path = "[path]",
-          luasnip = "[snip]",
-        },
-      }),
-    },
-  }
-  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-  cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
-  )
-end
-
 return {
   -- Autocompletion
   'hrsh7th/nvim-cmp',
@@ -72,5 +11,7 @@ return {
     'onsails/lspkind.nvim',
   },
   event = 'BufReadPre',
-  config = config,
+  config = function ()
+    require 'eps.cmp'
+  end,
 }
