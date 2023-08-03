bb584aaaebd402e1e124f5c3b1a9e83f2f553d4d
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-08-03 20:44:26 +0700

    add treesitter playground

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -17,7 +17,7 @@ local config = function ()
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
       -- Using this option may slow down your editor, and you may see some duplicate highlights.
       -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = false,
+      additional_vim_regex_highlighting = { "sql" },
     },
   }
 end
@@ -27,5 +27,8 @@ return {
   build = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
   end,
+  dependencies = {
+    'nvim-treesitter/playground',
+  },
   config = config,
 }
