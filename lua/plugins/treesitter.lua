490425f606fbbd4c4cf5c32b5730f18e4b12e8ab
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-08-05 14:09:45 +0700

    disable nvim treesitter auto install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ local config = function ()
 
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
+    auto_install = false,
 
     highlight = {
       -- `false` will disable the whole extension
@@ -17,7 +17,7 @@ local config = function ()
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
       -- Using this option may slow down your editor, and you may see some duplicate highlights.
       -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = { "sql" },
+      additional_vim_regex_highlighting = false,
     },
   }
 end
