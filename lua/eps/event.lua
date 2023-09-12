9f28978ecbc502541a7f8fdda659f55f000425d7
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-09-12 10:49:38 +0700

    refactor event module and add autocmd in CmdlineEnter

--- lua/eps/event.lua
+++ lua/eps/event.lua
@@ -1,6 +1,16 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = function (group_name, opts)
+  if opts then
+    return vim.api.nvim_create_augroup(group_name, opts)
+  else
+    return vim.api.nvim_create_augroup(group_name, { clear = false })
+  end
+end
 local cmd = vim.cmd
-vim.api.nvim_create_autocmd("VimEnter", {
-  group = vim.api.nvim_create_augroup("LoadAlpha", { clear = true }),
+
+-- Load Alpha when not given args
+autocmd("VimEnter", {
+  group = augroup("LoadAlpha", { clear = true }),
   callback = function ()
     local args = vim.v.argv
     if #args == 2 then
@@ -11,4 +21,23 @@ vim.api.nvim_create_autocmd("VimEnter", {
 )
 
 -- Reset marks each start open neovim
-vim.api.nvim_create_autocmd({ "BufRead" }, { command = ":delm a-zA-Z0-9", })
+autocmd({ "BufRead" }, { command = ":delm a-zA-Z0-9", })
+
+-- No relative Number on command mode
+require("eps.colors")
+autocmd("CmdlineEnter", {
+  group = augroup("NoRnuCmdline"),
+  callback = function ()
+    vim.wo.relativenumber = false
+    set_hl("LineNr", { fg = "#99a2b2" })
+    cmd.redraw()
+  end
+})
+
+autocmd("CmdlineLeave", {
+  group = augroup("NoRnuCmdline"),
+  callback = function ()
+    vim.wo.relativenumber = true
+    set_hl("LineNr", { fg = "yellow" })
+  end
+})
