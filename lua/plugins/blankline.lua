532f67c3e720dda30259dd784c6c01e38cb75b28
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-09-29 14:24:46 +0700

    update config ibl version 3

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -1,24 +1,12 @@
-local config = function ()
-  -- Enable `lukas-reineke/indent-blankline.nvim`
-  -- See `:help indent_blankline.txt`
-  vim.cmd [[highlight IndentBlanklineIndent guifg=#4d4d4d gui=nocombine]]
-
-  require('ibl').setup {
-    char = '┊',
-    show_trailing_blankline_indent = false,
-    space_char_blankline = " ",
-    show_current_context = true,
-    char_highlight_list = {
-      "IndentBlanklineIndent",
-    },
-    space_char_highlight_list = {
-      "IndentBlanklineIndent",
-    },
-  }
+local config = function()
+  require("ibl").setup({
+    indent = { char = "┊", tab_char = "┊" },
+    scope = { show_start = false, show_end = false, injected_languages = false },
+  })
 end
 
 return {
-  'lukas-reineke/indent-blankline.nvim',
-  main = 'ibl',
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   config = config,
 }
