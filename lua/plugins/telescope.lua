f1075e9df0c12d8163cb07540f439dc4e726b68a
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-10-14 22:42:13 +0700

    update tag version telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,7 +26,7 @@ end
 
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.3',
+  tag = '0.1.4',
   dependencies = { { 'nvim-lua/plenary.nvim' } },
   event = 'VeryLazy',
   config = config,
