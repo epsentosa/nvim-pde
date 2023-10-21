460ff84c6783f65d3f05ffa848c9fafb80da48a2
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2023-10-22 00:32:05 +0700

    move telescope file browser promt to bottom

--- lua/plugins/telescope-file-browser.lua
+++ lua/plugins/telescope-file-browser.lua
@@ -8,7 +8,7 @@ local config = function ()
         sorting_strategy = "ascending",
         layout_config = {
           horizontal = {
-            prompt_position = "top",
+            prompt_position = "bottom",
           },
         },
         grouped = true,
