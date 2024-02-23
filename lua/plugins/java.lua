8ff1d9b49ce775787d5c5f84447482d61651c612
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2024-02-23 16:00:10 +0700

    update java lsp config

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,68 +1,60 @@
+local settings = {
+  java = { -- https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+    implementationsCodeLens = { enabled = true },
+    inlayhints = {
+      parameterNames = { enabled = true },
+    },
+    referencesCodeLens = { enabled = true },
+    signatureHelp = { enabled = true },
+    symbols = {
+      includeSourceMethodDeclarations = true,
+    },
+  },
+}
+
 local config = function ()
-  local workspace_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+  capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+  local handlers = {
+    ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+      border = "rounded",
+    }),
+    ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+      border = "rounded",
+    }),
+  }
+
+  local navic = require("nvim-navic")
+
+  local on_attach = function(client, bufnr)
+    if client.server_capabilities.documentSymbolProvider then
+      navic.attach(client, bufnr)
+    end
+    local nmap = function(keys, func, desc)
+      if desc then
+        desc = "LSP: " .. desc
+      end
+      vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+    end
+
+    local lsp_mappings =  require("eps.keymaps").lsp_mappings
+    for _, params in ipairs(lsp_mappings) do
+      local keys, func, desc = unpack(params)
+      nmap(keys, func, desc)
+    end
+  end
 
   local config = {
-    cmd = {
-      'java',
-      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-      '-Dosgi.bundles.defaultStartLevel=4',
-      '-Declipse.product=org.eclipse.jdt.ls.core.product',
-      '-Dlog.protocol=true',
-      '-Dlog.level=ALL',
-      '-Xms1g',
-      '--add-modules=ALL-SYSTEM',
-      '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-      '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-      '-jar', '/usr/local/Cellar/jdtls/1.33.0/libexec/plugins/org.eclipse.equinox.launcher_1.6.700.v20231214-2017.jar',
-      '-configuration', '/usr/local/Cellar/jdtls/1.33.0/libexec/config_mac/',
-      '-data', vim.fn.expand('~/.cache/jdtls-workspace') .. workspace_dir
-    },
-    root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'gradlew' }),
-    single_file_support = true,
-    capabilities = capabilities,
-    settings = {
-      java = {
-      }
-    },
-    init_options = {
-      bundles = {}
-    },
+      cmd = {'/usr/local/bin/jdtls'},
+      root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+      capabilities = capabilities,
+      handlers = handlers,
+      settings = settings,
+      on_attach = on_attach,
   }
   require('jdtls').start_or_attach(config)
 
-  -- need to write again for keymap
-  local telescope = require('telescope.builtin')
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { desc = desc })
-  end
-
-  nmap('<leader>r', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', telescope.lsp_references, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', telescope.lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', telescope.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
 end
 
 return {
