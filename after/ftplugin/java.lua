fafdc11b34a0aeab54049672371dfc4e26051020
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2024-02-27 00:02:22 +0700

    update jdtls config

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -7,7 +7,6 @@ local jdtls = require("jdtls")
 local jdtls_capability = jdtls.extendedClientCapabilities
 jdtls_capability.resolveAdditionalTextEditsSupport = true
 
-local workspace_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local handlers = {
@@ -40,22 +39,8 @@ local on_attach = function(client, bufnr)
 end
 
 local config = {
-  cmd = {
-    'java',
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xms1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-    '-jar', '/usr/local/Cellar/jdtls/1.33.0/libexec/plugins/org.eclipse.equinox.launcher_1.6.700.v20231214-2017.jar',
-    '-configuration', '/usr/local/Cellar/jdtls/1.33.0/libexec/config_mac/',
-    '-data', vim.fn.expand('~/.cache/jdtls-workspace') .. workspace_dir
-  },
-  root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'gradlew' }),
+  cmd = {'/usr/local/bin/jdtls'},
+  root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
   single_file_support = true,
   capabilities = capabilities,
   handlers = handlers,
