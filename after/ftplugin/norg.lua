bbf9f952f2dc429fea7d94c7966d388c70ea0941
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2024-04-13 16:22:52 +0700

    update opts folding fillchars norg

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -3,5 +3,6 @@ vim.g.maplocalleader = " "
 local set = vim.opt_local
 set.conceallevel = 2
 set.concealcursor = "nc"
-vim.opt.listchars = {}
+set.listchars = {}
 set.tabstop = 2
+set.fillchars = { eob = "-", fold = " " }
