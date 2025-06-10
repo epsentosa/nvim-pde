4d28a94a1f4382a5bc9ae382323031397ba28d59
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2025-06-10 08:04:58 +0700

    fix go override vim.diagnostic.config

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -2,11 +2,11 @@ return {
   "ray-x/go.nvim",
   dependencies = {  -- optional packages
     "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
   },
   config = function()
-    require("go").setup()
+    require("go").setup({
+      diagnostic = false,
+    })
   end,
   event = {"CmdlineEnter"},
   ft = {"go", 'gomod'},
