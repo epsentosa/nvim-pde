b7a6ace383fb8a0df3aa49e571f2bf845dee77c8
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2025-12-21 23:06:39 +0700

    adjust treesitter ensure installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,8 @@
 local config = function ()
   require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "javascript", "typescript", "c", "lua", "rust", "python", "go" },
-    modules = { "javascript", "typescript", "c", "lua", "rust", "python", "go" },
+    ensure_installed = { "javascript", "typescript", "c", "lua", "rust", "python", "go", "sql", "yaml" },
+    modules = { "javascript", "typescript", "c", "lua", "rust", "python", "go", "sql", "yaml" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
