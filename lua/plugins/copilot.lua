ee9a480ef3e3023e0edfdddab74decb51e051d81
Eko Putra Sentosa <ekoputrasentosa@gmail.com>
2025-12-28 22:42:49 +0700

    add nes on copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,26 +1,42 @@
-local config = function ()
-  require('copilot').setup({
-    suggestion = {
-      enabled = not vim.g.ai_cmp,
-      auto_trigger = true,
-      hide_during_completion = vim.g.ai_cmp,
-      keymap = {
-        accept = "<Tab>",
-        next = "<M-]>",
-        prev = "<M-[>",
-        dismiss = "<C-]>",
-      },
-    },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
-    },
-  })
+local config = function()
+	require("copilot").setup({
+		suggestion = {
+			enabled = not vim.g.ai_cmp,
+			auto_trigger = true,
+			hide_during_completion = vim.g.ai_cmp,
+			keymap = {
+				accept = "<Tab>",
+				next = "<M-]>",
+				prev = "<M-[>",
+				dismiss = "<C-]>",
+			},
+		},
+		nes = {
+			enabled = true,
+			keymap = {
+				accept_and_goto = "<Tab>",
+				accept = false,
+				dismiss = "<Esc>",
+			},
+		},
+		panel = { enabled = false },
+		filetypes = {
+			markdown = true,
+			help = true,
+		},
+	})
 end
 
 return {
-  "zbirenbaum/copilot.lua",
-  config = config,
-  cmd = "Copilot",
+	"zbirenbaum/copilot.lua",
+	dependencies = {
+		{
+			"copilotlsp-nvim/copilot-lsp",
+			init = function()
+				vim.g.copilot_nes_debounce = 500
+			end,
+		},
+	},
+	config = config,
+	cmd = "Copilot",
 }
